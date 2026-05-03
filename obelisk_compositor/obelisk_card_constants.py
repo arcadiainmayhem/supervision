82f8d8f54df6501e098e8858304fc99db2d96b73
arcadiainmayhem <doodleypants@gmail.com>
2026-05-03 13:26:54 +0800

    full pipeline refactor

--- obelisk_compositor/obelisk_card_constants.py
+++ obelisk_compositor/obelisk_card_constants.py
@@ -23,5 +23,36 @@ ELEMENT_POSITIONS = {
     "accessory":  (int(CARD_WIDTH * 0.60), int(CARD_HEIGHT * 0.10), int(CARD_WIDTH * 0.80), int(CARD_HEIGHT * 0.25)),
 }
 
+CHASE_POSITIONS_1 = {
+    "background": (0, 0, CARD_WIDTH, CARD_HEIGHT),
+    "border":     (0, 0, CARD_WIDTH, CARD_HEIGHT),
+    "face":       (int(CARD_WIDTH * 0.25), int(CARD_HEIGHT * 0.15), int(CARD_WIDTH * 0.75), int(CARD_HEIGHT * 0.75)),
+    "eye_1":      (int(CARD_WIDTH * 0.30), int(CARD_HEIGHT * 0.30), int(CARD_WIDTH * 0.47), int(CARD_HEIGHT * 0.42)),
+    "eye_2":      (int(CARD_WIDTH * 0.53), int(CARD_HEIGHT * 0.30), int(CARD_WIDTH * 0.70), int(CARD_HEIGHT * 0.42)),
+    "nose":       (int(CARD_WIDTH * 0.40), int(CARD_HEIGHT * 0.44), int(CARD_WIDTH * 0.60), int(CARD_HEIGHT * 0.56)),
+    "mouth":      (int(CARD_WIDTH * 0.33), int(CARD_HEIGHT * 0.58), int(CARD_WIDTH * 0.67), int(CARD_HEIGHT * 0.68)),
+    "ear_1":      (int(CARD_WIDTH * 0.10), int(CARD_HEIGHT * 0.30), int(CARD_WIDTH * 0.25), int(CARD_HEIGHT * 0.50)),
+    "ear_2":      (int(CARD_WIDTH * 0.75), int(CARD_HEIGHT * 0.30), int(CARD_WIDTH * 0.90), int(CARD_HEIGHT * 0.50)),
+    "detail":     (int(CARD_WIDTH * 0.35), int(CARD_HEIGHT * 0.20), int(CARD_WIDTH * 0.65), int(CARD_HEIGHT * 0.32)),
+    "accessory":  (int(CARD_WIDTH * 0.60), int(CARD_HEIGHT * 0.10), int(CARD_WIDTH * 0.80), int(CARD_HEIGHT * 0.25)),    
+}
+
+CHASE_POSITIONS_2 = {
+    "background": (0, 0, CARD_WIDTH, CARD_HEIGHT),
+    "border":     (0, 0, CARD_WIDTH, CARD_HEIGHT),
+    "face":       (int(CARD_WIDTH * 0.25), int(CARD_HEIGHT * 0.15), int(CARD_WIDTH * 0.75), int(CARD_HEIGHT * 0.75)),
+    "eye_1":      (int(CARD_WIDTH * 0.30), int(CARD_HEIGHT * 0.30), int(CARD_WIDTH * 0.47), int(CARD_HEIGHT * 0.42)),
+    "eye_2":      (int(CARD_WIDTH * 0.53), int(CARD_HEIGHT * 0.30), int(CARD_WIDTH * 0.70), int(CARD_HEIGHT * 0.42)),
+    "nose":       (int(CARD_WIDTH * 0.40), int(CARD_HEIGHT * 0.44), int(CARD_WIDTH * 0.60), int(CARD_HEIGHT * 0.56)),
+    "mouth":      (int(CARD_WIDTH * 0.33), int(CARD_HEIGHT * 0.58), int(CARD_WIDTH * 0.67), int(CARD_HEIGHT * 0.68)),
+    "ear_1":      (int(CARD_WIDTH * 0.10), int(CARD_HEIGHT * 0.30), int(CARD_WIDTH * 0.25), int(CARD_HEIGHT * 0.50)),
+    "ear_2":      (int(CARD_WIDTH * 0.75), int(CARD_HEIGHT * 0.30), int(CARD_WIDTH * 0.90), int(CARD_HEIGHT * 0.50)),
+    "detail":     (int(CARD_WIDTH * 0.35), int(CARD_HEIGHT * 0.20), int(CARD_WIDTH * 0.65), int(CARD_HEIGHT * 0.32)),
+    "accessory":  (int(CARD_WIDTH * 0.60), int(CARD_HEIGHT * 0.10), int(CARD_WIDTH * 0.80), int(CARD_HEIGHT * 0.25)),    
+}
+
+
+
+
 Z_ORDER = ["background" , "border" , "face" , "eye_1" ,"eye_2" , "nose", "ear_1" , "ear_2" , "mouth", "detail" , "accessory"]
  
